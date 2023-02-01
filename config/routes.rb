63eceb9d74408710fb8226106cb38309f2566f61
github username <github email address>
2023-02-01 16:53:37 -0600

    partly through chapter 8

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  resources :products
-  get 'say/hello'
-  get 'say/goodbye'
+  root 'store#index' , as: 'store_index' #making the store index the root path. the 'as:' tells rails to create the
+                                          #store_index_path and store_index_url accessor methods(?)
+  # get 'store/index'   <--- not needed as we added the store index as the root path
+  resources :products #provides all the route methods by using 'resources'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
