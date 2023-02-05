957ceab3c4f172530e60042bdc4dd9b4097e0407
github username <github email address>
2023-02-05 10:21:30 -0600

    added cache functionality

--- test/controllers/store_controller_test.rb
+++ test/controllers/store_controller_test.rb
@@ -4,5 +4,6 @@ class StoreControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
     get store_index_url
     assert_response :success
+    assert_select 'nav.side_nav a' , minimum: 4
   end
 end
